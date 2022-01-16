eda706df5336175cf33c39ed9bf902cf432019b8
“[negar <miralaienegar@gmail.com>
2022-01-17 00:31:55 +0330

    added button to mobile navbar and completed styling it

--- vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/header/navbar/mobile-navbar.vue
+++ vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/header/navbar/mobile-navbar.vue
@@ -1,30 +1,29 @@
 <template>
-<section class="flex justify-between">
-      <section v-if="isSideNavbarOpen" @click="closeSideNavbar" class="mobile-navbar-bg cursor-pointer fixed w-screen bg-[rgba(0,0,0,0.6)] z-10">
-      <section class="mobile-navbar-content transform -translate-x-full transition duration-1000 ease-in-out flex gap-8 py-8 text-center flex-col items-center min-h-screen fixed z-20 top-0 right-0 w-full w-1/2 bg-customPink text-black">
-    <section class="overflow-hidden hover:bg-white w-full">
-      <router-link to="/about-us" class="mobile-navbar-element block py-4 transition ease-in-out delay-150 hover:scale-125" :class="{activeDesktopNavbarElement: $route.name==='about-us'}">درباره ما</router-link>
+  <section v-if="isSideNavbarOpen" @click="closeSideNavbar" class="mobile-navbar-bg cursor-pointer fixed w-screen bg-[rgba(0,0,0,0.6)] z-10">
+    <section class="mobile-navbar-content transform -translate-x-full transition duration-1000 ease-in-out flex gap-8 py-8 text-center flex-col items-center min-h-screen fixed z-20 top-0 right-0 w-full w-1/2 bg-customPink text-black">
+      <section class="overflow-hidden hover:bg-white w-full">
+        <router-link to="/about-us" class="mobile-navbar-element block py-4 transition ease-in-out delay-150 hover:scale-125" :class="{activeDesktopNavbarElement: $route.name==='about-us'}">درباره ما</router-link>
+      </section>
+      <section class="overflow-hidden hover:bg-white w-full">
+        <router-link to="/services" class="mobile-navbar-element block py-4 transition ease-in-out delay-150 hover:scale-125" :class="{activeDesktopNavbarElement: $route.name==='services'}">خدمات</router-link>
+      </section>
+      <section class="overflow-hidden hover:bg-white w-full">
+        <router-link to="/blog" class="mobile-navbar-element block py-4 transition ease-in-out delay-150 hover:scale-125" :class="{activeDesktopNavbarElement: $route.name==='blog'}">بلاگ</router-link>
+      </section>
+      <section class="overflow-hidden hover:bg-white w-full">
+        <router-link to="/contact-us" class="mobile-navbar-element block py-4 transition ease-in-out delay-150 hover:scale-125" :class="{activeDesktopNavbarElement: $route.name==='contact-us'}">ارتباط با ما</router-link>
+      </section>
+      <section class="overflow-hidden hover:bg-white w-full">
+        <button class="bg-customYellow rounded mobile-navbar-element mx-auto block py-4 px-6 transition ease-in-out delay-150 hover:scale-125">
+         ثبت درخواست ویزیت
+        </button>
+      </section>
     </section>
-    <section class="overflow-hidden hover:bg-white w-full">
-      <router-link to="/services" class="mobile-navbar-element block py-4 transition ease-in-out delay-150 hover:scale-125" :class="{activeDesktopNavbarElement: $route.name==='services'}">خدمات</router-link>
-    </section>
-     <section class="overflow-hidden hover:bg-white w-full">
-      <router-link to="/blog" class="mobile-navbar-element block py-4 transition ease-in-out delay-150 hover:scale-125" :class="{activeDesktopNavbarElement: $route.name==='blog'}">بلاگ</router-link>
-    </section>
-     <section class="overflow-hidden hover:bg-white w-full">
-      <router-link to="/contact-us" class="mobile-navbar-element block py-4 transition ease-in-out delay-150 hover:scale-125" :class="{activeDesktopNavbarElement: $route.name==='contact-us'}">ارتباط با ما</router-link>
-    </section>
-    <section class="overflow-hidden hover:bg-white w-full">
+  </section>
 
-    </section>
-  
-    </section>
-    </section>
-
-    <section>
+    <section class="md:hidden">
       <router-link to="/" class="text-left text-2xl md:text-3xl font-bold md:hidden">MED</router-link>
     </section>
-</section>
 </template>
 
 <script>
@@ -43,6 +42,7 @@ export default {
 <style scoped>
   .mobile-navbar-bg{
     min-height:200vh;
+    width: 200vw;
     /* background-color: #E35057; */
   }
   .activeDesktopNavbarElement{
