09908280534a825d3c20b3e70ef6a1d828de74cc
“[negar <miralaienegar@gmail.com>
2022-01-17 13:38:17 +0330

    made home-hero fully responsive

--- vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/home-partials/home-hero.vue
+++ vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/home-partials/home-hero.vue
@@ -1,12 +1,12 @@
 <template>
-    <section>
-        <section class="home-hero-text flex-col items-end space-14">
+    <section class="flex flex-col md:flex-row">
+        <section class="home-hero-text md:w-1/2 md:ml-8 flex-col items-end space-14">
             <small class="mt-8 text-customPink ">پزشکی</small>
             <h2 class="mt-4 font-bold text-xl">راه های مراقبت از سلامت</h2>
             <p class="mt-4">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
             <button class="bg-customPink mt-4 rounded block py-4 px-6 transition ease-in-out delay-150 hover:scale-125">خواندن</button>
         </section>
-        <section class="flex justify-center mt-4">
+        <section class="flex justify-center mt-8 md:mt-0 md:w-1/2 md:mr-8">
             <img src="https://media.istockphoto.com/vectors/thank-you-doctors-and-nurses-physicians-and-nurse-raised-hands-vector-id1220213713?k=20&m=1220213713&s=612x612&w=0&h=V6_coa6kzGiG1ha6UCp-SWrCJV_NRgPb5ISDarh6TSY=" alt="picture of nurses">
         </section>
     </section>
