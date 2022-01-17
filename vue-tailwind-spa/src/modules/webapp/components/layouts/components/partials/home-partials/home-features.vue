66037587c847b08422f105a47e4225eb4575dd37
“[negar <miralaienegar@gmail.com>
2022-01-17 16:38:34 +0330

    completed home-features and made it fully  responsive

--- vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/home-partials/home-features.vue
+++ vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/home-partials/home-features.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="mt-8">
+    <section class="mt-8 md:p-5%">
 
         <section class="md:w-1/2">
             <small class="mt-4 text-customPink">ویژگی ها</small>
@@ -7,11 +7,11 @@
             <p class="mt-4">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
         </section>
 
-        <section class="bg-customYellow mt-8 flex flex-col md:flex-row-reverse ">
-            <section class="md:w-1/2 flex items-center">
-                <img class="w-3/4" src="https://www.verywellhealth.com/thmb/JyVJmPFnrUe5mWbesKHsufm2qBE=/1997x1501/filters:no_upscale():max_bytes(150000):strip_icc()/surgeon-GettyImages-200140323-001-578ce4ab5f9b584d20ce3d9f.jpg" alt="surgery">
+        <section class="bg-customYellow p-2% ml-5% relative mt-8 flex flex-col md:flex-row-reverse ">
+            <section class="flex justify-center items-center md:w-2/6">
+                <img class="md:w-2/6 md:bottom-10% md:left-105% md:absolute" src="https://www.verywellhealth.com/thmb/JyVJmPFnrUe5mWbesKHsufm2qBE=/1997x1501/filters:no_upscale():max_bytes(150000):strip_icc()/surgeon-GettyImages-200140323-001-578ce4ab5f9b584d20ce3d9f.jpg" alt="surgery">
             </section>
-            <section class="flex flex-col md:w-1/2">
+            <section class="flex flex-col md:w-4/6">
                 <section class="flex items-center">
                     <section class="bg-customPink w-12 text-white h-12 rounded-3xl flex items-center justify-center">
                         <font-awesome-icon :icon="['fas', 'hospital']" class="text-white inline scale-150"/>
@@ -19,7 +19,7 @@
                     <p class="mr-4">ویزیت آنلاین</p>
                 </section>
                 <section class="flex flex-col items-start">
-                    <small class="mt-4 text-customGray">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</small>
+                    <small class="mt-4 text-customGray">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</small>
                     <button class="mt-4 bg-customOrange text-white rounded block py-4 px-6 transition ease-in-out delay-150 hover:scale-125">ادامه مطلب</button>
                 </section>
             </section>
