064c5447aea514fb728ab5cd9bcbc2d3e7410aef
“[negar <miralaienegar@gmail.com>
2022-01-19 03:00:11 +0330

    changed a method name

--- vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/header/navbar/navbar.vue
+++ vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/header/navbar/navbar.vue
@@ -1,29 +1,37 @@
-
 <template>
   <section class="flex items-center justify-between py-6">
-    <desktop-navbar/>
-    <section class="w-12 bg-customPink h-12 ml-auto rounded-3xl flex items-center justify-center md:hidden cursor-pointer">
-      <font-awesome-icon :icon="['fas', 'bars']" @click="toggleisMobileNavbarOpen()" class="text-black inline"/>
+    <desktop-navbar />
+    <section
+      class="w-12 bg-customPink h-12 ml-auto rounded-3xl flex items-center justify-center md:hidden cursor-pointer"
+    >
+      <font-awesome-icon
+        :icon="['fas', 'bars']"
+        @click="toggleIsMobileNavbarOpen()"
+        class="text-black inline"
+      />
     </section>
-    <mobile-navbar :isMobileNavbarOpen="isMobileNavbarOpen" @closeSideNavbar="closeSideNavbar"/>
+    <mobile-navbar
+      :isMobileNavbarOpen="isMobileNavbarOpen"
+      @closeSideNavbar="closeSideNavbar"
+    />
   </section>
   <router-view />
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      isMobileNavbarOpen : false
-    }
+  data() {
+    return {
+      isMobileNavbarOpen: false,
+    };
   },
-  methods:{
-    toggleisMobileNavbarOpen(){
+  methods: {
+    toggleIsMobileNavbarOpen() {
       this.isMobileNavbarOpen = !this.isMobileNavbarOpen;
     },
-    closeSideNavbar(payload){
+    closeSideNavbar(payload) {
       this.isMobileNavbarOpen = !this.isMobileNavbarOpen;
-    }
-  }
-}
+    },
+  },
+};
 </script>
