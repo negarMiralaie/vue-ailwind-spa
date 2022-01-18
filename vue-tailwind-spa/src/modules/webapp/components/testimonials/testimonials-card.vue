d446a238f02d4a240003fc954f00b71b16676724
“[negar <miralaienegar@gmail.com>
2022-01-19 03:07:02 +0330

    changed some p tags to small tags

--- vue-tailwind-spa/src/modules/webapp/components/testimonials/testimonials-card.vue
+++ vue-tailwind-spa/src/modules/webapp/components/testimonials/testimonials-card.vue
@@ -5,13 +5,13 @@
                 <font-awesome-icon :icon="icon" class="text-customPink text-3xl" :class="{ openingHours: title === 'Opening Hours' }"/>
             </section>
             <section class="flex flex-col mb-auto text-customDarkGray">
-                    <p>{{body}}</p>
+                    <small>{{body}}</small>
             </section>
             <section v-if="button" class="inline mb-auto ml-auto">
                 <button class="bg-customPink py-3 px-6 transition ease-in-out delay-150 hover:scale-105 hover:bg-customYellow rounded-3xl">{{button}}</button>
             </section> 
             <section v-else class="flex flex-col text-customLightGray">
-                <p> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است</p>
+                <small> لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است</small>
             </section>
     </section>
 </template>
