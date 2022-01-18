0e637e37de99b3f9e33a0a554af699c9f53d6612
“[negar <miralaienegar@gmail.com>
2022-01-19 03:20:04 +0330

    added hover effect to navbar

--- vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/header/navbar/desktop-navbar.vue
+++ vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/header/navbar/desktop-navbar.vue
@@ -1,39 +1,37 @@
 <template>
-  <section class="md:px-5% md:w-full flex">
+  <section class="desktopNavbar md:px-5% md:w-full flex">
     <section class="ml-auto w-1/2 hidden md:inline relative ">
       <button class="bg-customOrange font-bold hover:bg-customYellow hover:text-black text-white transition ease-in-out delay-150 hover:scale-105 rounded-3xl px-6 py-3 absolute right-1/4">
         ثبت درخواست ویزیت
       </button>
     </section>
 
-    <section
-    class="flex flex-col md:flex-row items-center justify-between hidden md:flex w-1/2"
-  >
+    <section class="flex flex-col md:flex-row items-center justify-between hidden md:flex w-1/2">
     <router-link
       to="/about-us"
-      class="py-3 transition ease-in-out delay-150 hover:scale-105 font-bold	"
+      class="desktopNavbarElement py-3 transition ease-in-out delay-150 hover:scale-105 font-bold	"
       :class="{ activeDesktopNavbarElement: $route.name === 'about-us' }"
       >درباره ما</router-link
     >
     <router-link
       to="/services"
-      class="py-3 transition ease-in-out delay-150 hover:scale-105 font-bold"
+      class="desktopNavbarElement py-3 transition ease-in-out delay-150 hover:scale-105 font-bold"
       :class="{ activeDesktopNavbarElement: $route.name === 'services' }"
       >خدمات</router-link
     >
     <router-link
       to="/blog"
-      class="py-3 transition ease-in-out delay-150 hover:scale-105 font-bold"
+      class="desktopNavbarElement py-3 transition ease-in-out delay-150 hover:scale-105 font-bold"
       :class="{ activeDesktopNavbarElement: $route.name === 'blog' }"
       >بلاگ</router-link
     >
     <router-link
       to="/contact-us"
-      class="py-3 transition ease-in-out delay-150 hover:scale-105 font-bold"
+      class="desktopNavbarElement py-3 transition ease-in-out delay-150 hover:scale-105 font-bold"
       :class="{ activeDesktopNavbarElement: $route.name === 'contact-us' }"
       >ارتباط با ما</router-link
     >
-    <router-link to="/" class="text-left text-2xl md:text-4xl font-extrabold"
+    <router-link to="/" class="desktopNavbarLogo text-left text-2xl md:text-4xl font-extrabold"
       >MED</router-link
     >
   </section>
