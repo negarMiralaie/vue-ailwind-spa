f77f19b7305d006327c6071bb2644b85340b35c3
“[negar <miralaienegar@gmail.com>
2022-01-19 05:10:27 +0330

    completed home-hero img sec

--- vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/home-partials/home-hero.vue
+++ vue-tailwind-spa/src/modules/webapp/components/layouts/components/partials/home-partials/home-hero.vue
@@ -8,11 +8,26 @@
         </section>
 
         <section class="min-h-80vw relative md:static md:min-h-0 flex justify-center items-center mt-8 md:mt-0 md:w-1/2 md:ml-8">
-            <section class="absolute bottom-0 right-0 w-4/6 h-4/6 md:w-44% md:h-3/6 lg:w-40% lg:h-2/5 md:top-0 md:right-0 bg-customPink z-11 shadow-lg rounded-bl-2xl"></section>
-            <section class="absolute bottom-0 right-0 w-1/6 h-1/6 md:w-25% md:h-1/5 lg:w-25% lg:h-1/5 xl:w-20% md:top-0 md:right-0 bg-red-700 z-11 rounded-bl-2xl"></section>
+            <section class="absolute bottom-0 right-0 w-4/6 h-4/6 md:w-44% md:h-3/6 lg:w-40% lg:h-45% md:top-0 md:right-0 bg-customPink z-12 shadow-lg rounded-bl-2xl"></section>
+            <section class="absolute bottom-0 right-0 w-1/6 h-1/6 md:w-25% md:h-1/5 lg:w-25% lg:h-1/5 xl:w-20% md:top-0 md:right-0 bg-red-700 z-12 rounded-bl-2xl"></section>
 
             <section class="w-4/5 md:w-full md:h-1/3 lg:w-80% lg:h-2/3 flex items-center justify-center bg-customYellow shadow-lg rounded-2xl relative">
                 <img src="../../../../../../../assetes/images/hero-img.png" alt="doctors picture" class="md:w-4/6 rounded-2xl md:absolute md:bottom-0">
+                <img class="absolute top-125% left-115% w-1/3 z-11" src="https://www.heathrowe.com/tuts/easypatternsimages/7.gif" alt="circle pattern">
+                <img class="absolute bottom-125% right-115% w-1/3 z-11" src="https://www.heathrowe.com/tuts/easypatternsimages/7.gif" alt="circle pattern">
+
+                <section class="shadow-lg rounded-2xl bg-white w-14 h-14 md:h-20 md:w-20 lg:w-32 lg:h-32 z-50 absolute top-15% right-105% lg:right-115% flex items-center justify-center">
+                        <section class="bg-blue-400 rounded-full w-12 h-12 md:w-14 md:h-14 lg:w-24 lg:h-24 flex items-center justify-center">
+                            <font-awesome-icon :icon="['fas', 'stethoscope']" class="text-white text-2xl lg:text-6xl z-60"/>
+                        </section>
+                </section>
+
+                <section class="shadow-lg rounded-2xl bg-white w-14 h-14 md:h-20 md:w-20 lg:w-32 lg:h-32 z-50 absolute bottom-110% left-105% md:left-110% lg:left-120% xl:left-110% flex items-center justify-center">
+                        <section class="bg-purple-400 rounded-full w-12 h-12 md:w-14 md:h-14 lg:w-24 lg:h-24 flex items-center justify-center">
+                            <font-awesome-icon :icon="['fas', 'notes-medical']" class="text-white text-2xl lg:text-6xl z-60"/>
+                        </section>
+                </section>
+
             </section>
         </section>
     </section>
